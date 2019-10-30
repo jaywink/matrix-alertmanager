edf6a6306c5defd457a089f8ccac86df1dd4630f
Jason Robinson <mail@jasonrobinson.me>
2019-10-30 23:34:28 +0200

    Add version param to docker_release script

--- docker_release.sh
+++ docker_release.sh
@@ -1,5 +1,20 @@
 #!/usr/bin/env bash
 
+set -e
+
+if [[ -z "$1" ]]; then
+    tag=latest
+else
+    tag=$1
+fi
+
 docker login
 docker build -t jaywink/matrix-alertmanager .
+docker push jaywink/matrix-alertmanager:${tag}
+
+if [[ "$tag" == "latest" ]]; then
+    exit
+fi
+
+docker tag jaywink/matrix-alertmanager:${tag} jaywink/matrix-alertmanager:latest
 docker push jaywink/matrix-alertmanager:latest
